859ae552a997db5e162f1cee3d0188e39ca98a68
shakirkhan1 <me.shakir1996@gmail.com>
2019-04-18 22:39:12 +0530

    updated few files

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host     = "localhost";     // your Host name
-$username = "root";          // your Mysql username
-$password = "";              // your Mysql password
-$db_name  = "groupchat";          // your Database name
-
-$con=mysqli_connect("$host", "$username", "$password","$db_name") or die("could not connect to server.");
-?>
